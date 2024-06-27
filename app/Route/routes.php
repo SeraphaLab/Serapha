7b9e820ae8b03a2567139165cf24de64d589c6e3
carry0987 <carry0987@users.noreply.github.com>
2024-06-27 20:44:40 +0800

    Simplify router creation

--- app/Route/routes.php
+++ app/Route/routes.php
@@ -1,9 +1,15 @@
 <?php
 /** @var \Serapha\Routing\Router $router */
 
-$router->addRoute('GET', '/', 'HomeController@index');
-$router->addRoute('GET', '/user/create', 'UserController@create');
-$router->addRoute('POST', '/user/create', 'UserController@store');
-$router->addRoute('GET', '/user/{id}', 'UserController@show');
-$router->addRoute('GET', '/login', 'AuthController@index');
-$router->addRoute('POST', '/login', 'AuthController'); // This will call index method as default
+use App\Controller\{
+    HomeController,
+    UserController,
+    AuthController
+};
+
+$router->get('/', HomeController::class); // This will call index method as default
+$router->get('/user/create', [UserController::class, 'create']);
+$router->post('/user/create', [UserController::class, 'store']);
+$router->addRoute('GET', '/user/{id}', [UserController::class, 'show']);
+$router->addRoute('GET', '/login', [AuthController::class, 'index']);
+$router->addRoute('POST', '/login', [AuthController::class]);
